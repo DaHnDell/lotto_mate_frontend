fc95bc1e225930422d97041a746695cec01567d7
jungyoonn <sophia7625@naver.com>
2025-04-14 14:04:36 +0900

    push 테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          도커 잘 되고 있을까...
         </a>
       </header>
     </div>
