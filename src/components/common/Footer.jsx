582a05f6c3b3722231f2abb57a3a9502785864da
jungyoonn <sophia7625@naver.com>
2025-04-15 17:47:30 +0900

    footer 파일만 만들었음

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../resources/css/style.css';
+
+const Footer = () => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default Footer;
